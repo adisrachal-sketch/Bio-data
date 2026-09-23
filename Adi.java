791c869379b4492e500960b8259bd768a80beb5b
adisrachal-sketch <adisrachal@gmail.com>
2026-09-23 09:22:43 +0700

    Create Adi.java

---
+++ Adi.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nama = "M adi setiawan";
+        String alamat = "bareng,sekar,Bojonegoro";
+        String nim = "261301011";
+        String fakultas = "Fakultas Sains Dan Teknik";
+        String prodi = "Sistem Komputer";
+        String ttl = "Bojonegoro,  26 juli 2007";
+
+        System.out.println("===== BIODATA MAHASISWA =====");
+        System.out.println("Nama      : " + nama);
+        System.out.println("Alamat    : " + alamat);
+        System.out.println("NIM       : " + nim);
+        System.out.println("Fakultas  : " + fakultas);
+        System.out.println("Prodi     : " + prodi);
+        System.out.println("TTL       : " + ttl);
+    }
+}
